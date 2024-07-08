31cab428877be81bb185adca5f23c53877dda58a
TheNobleDev <83643280+TheNobleDev@users.noreply.github.com>
2024-07-08 08:52:45 +0530

    Collate all external dependencies pt 2

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.20;
 
-import { INounsToken } from './interfaces/INounsToken.sol';
-import { INounsDaoProxy } from './interfaces/INounsDaoProxy.sol';
+import { INounsToken } from './external/nouns/interfaces/INounsToken.sol';
+import { INounsDaoProxy } from './external/nouns/interfaces/INounsDaoProxy.sol';
 
 contract Vault {
     address payable public owner;
@@ -24,11 +24,11 @@ contract Vault {
         return 1;
     }
 
-    function initialize(INounsToken _nounsToken, address _nounsDaoProxy) external {
+    function initialize(INounsToken _nounsToken, INounsDaoProxy _nounsDaoProxy) external {
         require(owner == address(0), 'Already initialized');
         owner = payable(msg.sender);
         nounsToken = _nounsToken;
-        nounsDaoProxy = INounsDaoProxy(_nounsDaoProxy);
+        nounsDaoProxy = _nounsDaoProxy;
     }
 
     function castVote(uint256 proposalId, uint8 support) external onlyOwner {
